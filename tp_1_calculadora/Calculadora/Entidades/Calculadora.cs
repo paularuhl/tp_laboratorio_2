0da7ffdf7d944facfdbd4da3b047a7f449e81664
Paula <Palu@DESKTOP-K0JUAIN>
2020-04-06 01:42:31 -0300

    add basic constructors and entities

---
+++ tp_1_calculadora/Calculadora/Entidades/Calculadora.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Linq;
+
+namespace Entidades
+{
+    public class Calculadora
+    {
+        public static double Operar(Numero num1, Numero num2, string operador)
+        {
+            double resultado;
+            switch (ValidarOperador(operador))
+            {
+                case "+":
+                    resultado = num1 + num2;
+                    break;
+                case "-":
+                    resultado = num1 - num2;
+                    break;
+                case "*":
+                    resultado = num1 * num2;
+                    break;
+                case "/":
+                    resultado = num1 / num2;
+                    break;
+            }
+            return resultado;
+        }
+
+        private static string ValidarOperador(string operador)
+        {
+            string[] operadores = { "+", "-", "*", "/" };
+            return operadores.Contains(operador) ? operador : operadores[0];
+        }
+    }
+}
---
+++ tp_1_calculadora/Calculadora/Entidades/Numero.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Entidades
+{
+    public class Numero
+    {
+        private double numero;
+
+        private string SetNumero
+        {
+            set
+            {
+                numero = ValidarNumero(value);
+            }
+        }
+
+        public Numero() : this(0)
+        {
+        }
+        public Numero(double numero)
+        {
+            SetNumero = numero.ToString();
+        }
+        public Numero(string strNumero)
+        {
+            SetNumero = strNumero;
+        }
+
+        private double ValidarNumero(string strNumero)
+        {
+            double numero;
+            double.TryParse(strNumero, out numero);
+            return numero;
+        }
+    }
+}
