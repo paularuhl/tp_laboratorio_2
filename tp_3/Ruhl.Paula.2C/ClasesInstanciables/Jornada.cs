c388022609be75bbcd21fbb0ed866ff363ef376c
Paula <A308839@santandertecnologia.com.ar>
2020-06-01 04:15:45 -0300

    JORNADA ATRIBUTOS

--- tp_3/Ruhl.Paula.2C/ClasesInstanciables/Jornada.cs
+++ tp_3/Ruhl.Paula.2C/ClasesInstanciables/Jornada.cs
@@ -3,10 +3,51 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using ClasesInstanciables;
 
 namespace ClasesInstanciables
 {
     public class Jornada
     {
+        private List<Alumno> alumnos;
+        private Universidad.EClases clase;
+        private Profesor instructor;
+
+        public List<Alumno> Alumnos
+        {
+            get
+            {
+                return this.alumnos;
+            }
+            set
+            {
+                this.alumnos = value;
+            }
+        }
+
+        public Universidad.EClases Clase
+        {
+            get
+            {
+                return this.clase;
+            }
+            set
+            {
+                this.clase = value;
+            }
+        }
+
+        public Profesor Instructor
+        {
+            get
+            {
+                return this.instructor;
+            }
+            set
+            {
+                this.instructor = value;
+            }
+        }
+
     }
 }
