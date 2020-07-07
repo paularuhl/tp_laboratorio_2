092deaad7bd92fd6127097725146b3acb0270f9c
Paula <paularuhl97@gmail.com>
2020-07-07 14:17:39 -0300

    add comment

--- Recuperatorio/tp_4/Entidades/TrackingIdRepetidoException.cs
+++ Recuperatorio/tp_4/Entidades/TrackingIdRepetidoException.cs
@@ -9,12 +9,12 @@ namespace Entidades
     public class TrackingIdRepetidoException : Exception
     {
         /// <summary>
-        /// 
+        /// Constructor de excepcion que solo recibe mensaje
         /// </summary>
         /// <param name="mensaje"></param>
         public TrackingIdRepetidoException(string mensaje) : this(mensaje, null) { }
         /// <summary>
-        /// 
+        /// Constructor de excepcion que recibe mensaje y una inner exception
         /// </summary>
         /// <param name="mensaje"></param>
         /// <param name="inner"></param>
